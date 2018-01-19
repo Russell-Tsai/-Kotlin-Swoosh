fe1d3d455db6b232f808f50914b73f4c134c971d
RUSSELL.TSAI <vanilla.love10@gmail.com>
2018-01-19 23:31:11 +0800

    lesson 3

--- app/src/main/java/russell/personal/demo/com/swoosh/MainActivity.kt
+++ app/src/main/java/russell/personal/demo/com/swoosh/MainActivity.kt
@@ -1,12 +1,19 @@
 package russell.personal.demo.com.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedBtn.setOnClickListener {
+            var leagueIntent : Intent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
